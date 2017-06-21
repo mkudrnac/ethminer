68f2e12af5219117fee660470caa0960691673f2
Miroslav Kudrnac <miroslav.kudrnac@rohlik.cz>
2017-06-21 14:09:40 +0200

    + f

--- libethash-cuda/fnv.cuh
+++ libethash-cuda/fnv.cuh
@@ -6,13 +6,13 @@
 #define FNV_PRIME	0x01000193
 
 __device__ __forceinline__
-const uint32_t fnv(const uint32_t x, const uint32_t y) const
+const uint32_t fnv(const uint32_t x, const uint32_t y)
 {
     return x * FNV_PRIME ^ y;
 }
 
 __device__ __forceinline__
-const uint4 fnv4(const uint4 a, const uint4 b) const
+const uint4 fnv4(const uint4 a, const uint4 b)
 {
 	uint4 c;
 	c.x = a.x * FNV_PRIME ^ b.x;
@@ -23,7 +23,7 @@ const uint4 fnv4(const uint4 a, const uint4 b) const
 }
 
 __device__ __forceinline__
-const uint32_t fnv_reduce(uint4 v) const 
+const uint32_t fnv_reduce(uint4 v)
 {
 	return fnv(fnv(fnv(v.x, v.y), v.z), v.w);
 }
