48fd6a49ea25a69ad6282c02762954cce493afd4
Miroslav Kudrnac <miroslav.kudrnac@rohlik.cz>
2017-06-22 17:23:23 +0200

    + f

--- libethash-cuda/ethash_cuda_miner_kernel.cu
+++ libethash-cuda/ethash_cuda_miner_kernel.cu
@@ -12,9 +12,13 @@
 #include "keccak.cuh"
 #include "dagger_shuffled.cuh"
 
+#define TPB		896
+#define BPSM	1
+
 #define copy(dst, src, count) for (int i = 0; i != count; ++i) { (dst)[i] = (src)[i]; }
 
-__global__ void 
+__global__ void
+__launch_bounds__(TPB, BPSM)
 ethash_search(
 	volatile uint32_t* g_output,
 	uint64_t start_nonce
@@ -47,6 +51,7 @@ void run_ethash_search(
 #define NODE_WORDS (64/4)
 
 __global__ void
+__launch_bounds__(128, 7)
 ethash_calculate_dag_item(uint32_t start)
 {
 	uint32_t const node_index = start + blockIdx.x * blockDim.x + threadIdx.x;
